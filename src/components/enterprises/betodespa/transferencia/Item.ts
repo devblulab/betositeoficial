77afcf4bb5f8b3dfc2d7d89bc55b02bfd17afb3d
devblulab <116671864+devblulab@users.noreply.github.com>
2025-06-25 15:42:01 -0300

    Update Item.ts

--- src/components/enterprises/betodespa/transferencia/Item.ts
+++ src/components/enterprises/betodespa/transferencia/Item.ts
@@ -3,45 +3,64 @@ import { Timestamp } from 'firebase/firestore';
 interface Item {
   id:string,
     cliente: string,
-    
+
     status: string,
     quantidade: number;
     imagemUrls: string[],
     concluido: false,
+
+
     placa: string,
     renavam: string,
     crv: string,
-    valordevenda:  string;
-   bairrocomprador: string,
-    
+    chassi:string,
+    modelo:string,
 
-    nomevendedor: string,
-    cpfvendedor: string,
-    enderecovendedor: string,
-    complementovendedor: string,
-    municipiovendedor: string,
-    emailvendedor: string,
+
+    valordevenda:  string;
+   bairroempresa: string,
+    cargo3:string,
+   cargo:string,
+   nomesocio1: string,
+    cpfsocio1: string,
+    enderecosocio1: string,
+    complementosocio1: string,
+    municipiosocio1: string,
+    emailsocio1: string,
+    cepsocio1: string;
+    celtelsocio1: string,
+    cargo2:string,
+   nomesocio2: string,
+   nomesocio3: string,
+    cpfsocio2: string,
+    cpfsocio3: string,
+    enderecosocio2: string,
+    complementosocio2: string,
+    municipiosocio2: string,
+    emailsocio2: string,
+    cepsocio2: string;
+    celtelsocio2: string,
     dataCriacao: string | Timestamp;
-    nomecomprador: string,
-    cpfcomprador: string,
-    enderecocomprador: string,
-    complementocomprador: string,
-    municipiocomprador: string,
-    emailcomprador: string,
-   celtelcomprador: string,
-   cepvendedor: string;
-    cepcomprador: string;
+    nomeempresa: string,
+    cpfempresa: string,
+    enderecoempresa: string,
+    complementoempresa: string,
+    municipioempresa: string,
+    emailempresa: string,
+   celtelempresa: string,
+
+    cepempresa: string;
     tipo: string;
     cnpjempresa: string;
-    nomeempresa: string;
 
-    celtelvendedor: string,
+
+
     signature?: string; 
- 
-  
+
+
 
 }
-    
+
 
 
 export default Item;
