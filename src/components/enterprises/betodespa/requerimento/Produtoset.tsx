1a23631197d3e3efe7a48f62d3e7c0968d82ca3e
devblulab <116671864+devblulab@users.noreply.github.com>
2025-06-26 15:22:32 -0300

    Update Produtoset.tsx

--- src/components/enterprises/betodespa/requerimento/Produtoset.tsx
+++ src/components/enterprises/betodespa/requerimento/Produtoset.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import ListPost from './ListPost';
 import ItemList from './ItemList';
 import { Timestamp } from 'firebase/firestore'; 
