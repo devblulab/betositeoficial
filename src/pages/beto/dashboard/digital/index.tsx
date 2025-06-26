321a9dc3082a76bdfad90fc63fe933e593c2a2b9
devblulab <116671864+devblulab@users.noreply.github.com>
2025-06-26 15:21:42 -0300

    Update index.tsx

--- src/pages/beto/dashboard/digital/index.tsx
+++ src/pages/beto/dashboard/digital/index.tsx
@@ -1,5 +1,6 @@
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
 import {
   Typography, Paper, Card, TextField, Button, CircularProgress, IconButton,
   List, ListItem, ListItemText, Divider, Grid, Avatar, Snackbar, Box, Container,
