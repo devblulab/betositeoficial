6b691e1ae21a0da3f5a67a7811a70bf2e81e356c
devblulab <116671864+devblulab@users.noreply.github.com>
2025-07-02 10:45:56 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.enygma.betoapp',      // Mude para o seu ID único
   appName: 'Despachante Beto',      // Nome do app (pode ser qualquer um)
-  webDir: 'out',                    // <-- Usa a pasta gerada por `npm run export`
+ webDir: '.next',                     // <-- Usa a pasta gerada por `npm run export`
   server: {
     androidScheme: 'https'          // Mantém https (opcional)
   }
